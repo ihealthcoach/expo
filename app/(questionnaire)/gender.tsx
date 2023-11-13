19bd482d45028435e78be10471139a3dd800e6e4
Zvezke <superur@gmail.com>
2023-11-13 17:39:51 +0100

    Added gradient bg to all pages in GS and Q

--- app/(questionnaire)/gender.tsx
+++ app/(questionnaire)/gender.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import React, { useState } from "react";
 
 import Button from "@/components/Button";
@@ -9,49 +9,54 @@ const QGender = () => {
   const [value, setValue] = useState("");
 
   return (
-    <View className="flex-1 items-center justify-center bg-[#66eed1]">
-      <View className="absolute top-0 w-full items-center py-3">
-        <Text className="h-5">Progress Bar</Text>
-      </View>
-      <View className="mb-36 w-full">
-        <View className="items-center">
-          <Text className="mb-2 text-xl font-semibold italic leading-tight text-primary-700">
-            What is your
+    <ImageBackground
+      className="h-full w-full flex-1"
+      source={require("@/assets/images/imageBg.png")}
+    >
+      <View className="flex-1 items-center justify-center">
+        <View className="absolute top-0 w-full items-center py-3">
+          <Text className="h-5">Progress Bar</Text>
+        </View>
+        <View className="mb-36 w-full">
+          <View className="items-center">
+            <Text className="mb-2 text-xl font-semibold italic leading-tight text-primary-700">
+              What is your
+            </Text>
+            <Text className="-mt-1 mb-6 font-interExtraBold text-5xl leading-tight text-primary-900">
+              Gender?
+            </Text>
+            {/* <RadioButton /> */}
+            <CardRadioButton
+              headline={"Male"}
+              body={null}
+              name={"male"}
+              value={value}
+              setValue={setValue}
+            />
+            <CardRadioButton
+              headline={"Female"}
+              body={null}
+              name={"female"}
+              value={value}
+              setValue={setValue}
+            />
+            <CardRadioButton
+              headline={"I'd rather not say"}
+              body={null}
+              name={"unknown"}
+              value={value}
+              setValue={setValue}
+            />
+          </View>
+        </View>
+        <View className="absolute bottom-0 w-full px-4 pb-12">
+          <Button text={"Continue"} path={"mainGoal"} />
+          <Text className="mb-4 text-center font-interMedium text-base text-primary-700">
+            Back
           </Text>
-          <Text className="-mt-1 mb-6 font-interExtraBold text-5xl leading-tight text-primary-900">
-            Gender?
-          </Text>
-          {/* <RadioButton /> */}
-          <CardRadioButton
-            headline={"Male"}
-            body={null}
-            name={"male"}
-            value={value}
-            setValue={setValue}
-          />
-          <CardRadioButton
-            headline={"Female"}
-            body={null}
-            name={"female"}
-            value={value}
-            setValue={setValue}
-          />
-          <CardRadioButton
-            headline={"I'd rather not say"}
-            body={null}
-            name={"unknown"}
-            value={value}
-            setValue={setValue}
-          />
         </View>
       </View>
-      <View className="absolute bottom-0 w-full px-4 pb-12">
-        <Button text={"Continue"} path={"mainGoal"} />
-        <Text className="mb-4 text-center font-interMedium text-base text-primary-700">
-          Back
-        </Text>
-      </View>
-    </View>
+    </ImageBackground>
   );
 };
 export default QGender;
