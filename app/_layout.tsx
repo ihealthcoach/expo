4d6785607d01ff9953e3384e66eda4c4bec0c417
kror-shack <tariqfouzan1@gmail.com>
2024-07-05 16:59:32 +0300

    feat: add rq persistence

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,18 +8,44 @@ import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { QuestionnaireProvider } from "@/context/useQuestionnaire";
+import { onlineManager } from "@tanstack/react-query";
+import NetInfo from "@react-native-community/netinfo";
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
+const persister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+  throttleTime: 3000,
+});
+
 export default function Layout() {
   const { fontsLoaded, fontError } = loadFonts();
+
+  useEffect(() => {
+    return NetInfo.addEventListener((state) => {
+      const status = !!state.isConnected;
+      onlineManager.setOnline(status);
+    });
+  }, []);
 
   if (!fontsLoaded && !fontError) {
     return null;
   }
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      onSuccess={() =>
+        queryClient
+          .resumePausedMutations()
+          .then(() => queryClient.invalidateQueries())
+      }
+      persistOptions={{ persister }}
+      client={queryClient}
+    >
       <GestureHandlerRootView className="flex-1">
         {/* <BottomSheetModalProvider> */}
         {/* <View className="h-full w-full flex-1 bg-red-500"> */}
@@ -51,6 +77,6 @@ export default function Layout() {
         {/* </View> */}
         {/* </BottomSheetModalProvider> */}
       </GestureHandlerRootView>
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   );
 }
