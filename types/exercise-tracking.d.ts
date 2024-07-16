d321471196c857b48c643c059c2e96666c07420d
kror-shack <tariqfouzan1@gmail.com>
2024-07-16 17:42:35 +0300

    update: exercise-tracking type

--- types/exercise-tracking.d.ts
+++ types/exercise-tracking.d.ts
@@ -17,9 +17,16 @@ export type ExerciseDetails = {
 export type Set = {
   id: string; // Unique identifier for the set
   exercise_details_id: string; // Foreign key to the ExerciseDetails table
-  weight: number[]; // Weight lifted in the set
+  weight: number; // Weight lifted in the set
   type: string; // TODO: change to enum
-  reps: number[]; // Number of repetitions in the set
+  reps: number; // Number of repetitions in the set
+  completed: boolean;
   created_at: Date;
   updated_at: Date;
+};
+
+export type ExerciseDetailsWithSets = {
+  exercise_details: ExerciseDetails; // The ExerciseDetails object
+  exercise: Exercise; // The Exercise object
+  sets: Set[]; // Array of sets linked to the ExerciseDetails
 };
